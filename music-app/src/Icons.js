773fe1144002986a8af71b53c83924d941fac145
Kihangu Waweru <kw817373@gmail.com>
2025-03-09 22:39:40 -0400

    Completed Icons

---
+++ music-app/src/Icons.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import {Heart, Shuffle, Home, Repeat, Category, Playlist, PlayBack, Skip, Play, Pause, Back, Search, Add, Settings} from './heart';
+
+function Icons() {
+  return (
+    <div>
+      <Heart />
+      <Shuffle />
+      <Home />
+      <Repeat />
+      <Category />
+      <Playlist />
+      <PlayBack />
+      <Skip />
+      <Play />
+      <Pause />
+      <Back />
+      <Search />
+      <Add />
+      <Settings />
+    </div>
+  );
+}
+
+export default Icons;
