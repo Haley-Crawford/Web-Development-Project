148a87e459e4789fde12c9152676400b35c7ee4d
Destiny Ryals <dcryals21@students.desu.edu>
2025-03-28 23:48:50 -0400

    created NavBar

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,20 @@
 //import logo from './logo.svg'
 import SignUp from './components/sign up.jsx'
-import LogIn from './components/login.jsx'
-import SongCard from './components/song card.jsx'
+import LogIn from './components/Login.jsx'
+import SongCard from './components/Song Card.jsx'
 import Carousel from './components/song carousel.jsx'
 import ArtistInfo from './components/artist info.jsx'
 import ArtistPage from './components/artist page.jsx'
 import SongsComponent from './components/artist page - songs.jsx'
+import NavBar from './components/NavBar.jsx'
 import './App.css'
 
 function App() {
   return (
+    <>
+    <NavBar/>
     <ArtistPage />
+    </>
   )
 }
 
