5799627ef5b7986109ece234c2075e814212195d
Haley Crawford <hpcrawford23@students.desu.edu>
2025-04-13 16:15:38 -0400

    minor changes

--- my-app/src/components/Artist Page/Songs/Songs.jsx
+++ my-app/src/components/Artist Page/Songs/Songs.jsx
@@ -42,20 +42,22 @@ function GenerateSong(n) {
 
 export function Songs() {
 
-    const songs = [
-        {id: 1, title: `Song ${this.id}`, album: 'Album Name', release: new Date().getFullYear(), isLiked: false},
-        {id: 2, title: `Song ${this.id}`, album: 'Album Name', release: new Date().getFullYear(), isLiked: false},
-        {id: 3, title: `Song ${this.id}`, album: 'Album Name', release: new Date().getFullYear(), isLiked: false},
-        {id: 4, title: `Song ${this.id}`, album: 'Album Name', release: new Date().getFullYear(), isLiked: false},
-        {id: 5, title: `Song ${this.id}`, album: 'Album Name', release: new Date().getFullYear(), isLiked: false}
-    ]
+    const [songs, setSongs] = useState([
+        {id: 1, title: `Song 1`, album: 'Album Name', release: new Date().getFullYear(), isLiked: false},
+        {id: 2, title: `Song 2`, album: 'Album Name', release: new Date().getFullYear(), isLiked: false},
+        {id: 3, title: `Song 3`, album: 'Album Name', release: new Date().getFullYear(), isLiked: false},
+        {id: 4, title: `Song 4`, album: 'Album Name', release: new Date().getFullYear(), isLiked: false},
+        {id: 5, title: `Song 5`, album: 'Album Name', release: new Date().getFullYear(), isLiked: false}
+    ])
 
     
     //const [likedSongs, setLikedSongs] = useState([])
 
 
-    const handleLike = (song) => {
-        song.isLiked = !song.isLiked
+    const handleLike = (id) => {
+        const updatedSongs = songs.map(song => song.id === id ? { ...song, isLiked: !song.isLiked } : song)
+
+        setSongs(updatedSongs)
         // setLikedSongs([...songs, song])
         // console.log(likedSongs)
     }
@@ -64,16 +66,16 @@ export function Songs() {
         <div className={styles.song_container}>
             <h2 className={styles.title}>Popular Songs</h2>
             <ol className={styles.song_list}>    
-                {songs.map((song, index) => (
+                {songs.map((song) => (
                     <li className={styles.song} key={song.id}>
                         <div className={styles.img_div}>
                             <img src='https://picsum.photos/40' alt='' className={styles.song_img}/>
                         </div>
                         <div className={styles.song_info}>
-                            <p className={styles.song_name}>Song Name</p>
-                            <p className={styles.album_info}>Album Name • {new Date().getMonth()} {new Date().getFullYear()}</p>
+                            <p className={styles.song_name}>{song.title}</p>
+                            <p className={styles.album_info}>{song.album} • {new Date().getMonth()} {song.release}</p>
                         </div>
-                        <button className={styles.btn} onClick={() => handleLike(song)}>
+                        <button className={styles.btn} onClick={() => handleLike(song.id)}>
                             <Heart className={`${styles.icon} ${song.isLiked ? styles.like : ''}`}/>
                         </button>
                     </li>
