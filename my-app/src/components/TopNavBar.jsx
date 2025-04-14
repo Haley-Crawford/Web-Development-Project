fd671d7ecb6c94a9810292898ad9a6fe2d49cfac
IRO05 <isaacedwardrobinson@gmail.com>
2025-04-14 15:28:52 -0400

    working on search songs

--- my-app/src/components/TopNavBar.jsx
+++ my-app/src/components/TopNavBar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import './components.css';
 import SearchBar from "./SearchBar";
 
-const TopNavBar = () => {
+const TopNavBar = ({searchResults, setSearchResults, isSearching, setIsSearching}) => {
   return (
     <div className="top-navbar">
-      <SearchBar onSearch={(query) => console.log("Searching for:", query)} />
+      <SearchBar searchResults={searchResults} setSearchResults={setSearchResults} isSearching={isSearching} setIsSearching={setIsSearching} />
       <div className="app-name">
       <h1>Music App</h1>
       </div>
